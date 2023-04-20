be0ab95b7e1521ed1347fbf25c089fc677e7687b
khairatsh <85008480+khairatsh@users.noreply.github.com>
2023-04-20 23:38:01 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,495 @@
+
+    var drhossam = document.getElementById("drhossam");
+    var nilay = document.getElementById("nilay");
+    var sarhan = document.getElementById("sarhan");
+    var ramy = document.getElementById("ramy");
+    var asar = document.getElementById("asar");
+    var rania = document.getElementById("rania");
+    var ingy = document.getElementById("ingy");
+    var randa = document.getElementById("randa");
+    var mona = document.getElementById("mona");
+    var mai = document.getElementById("mai");
+    var maha = document.getElementById("maha");
+    var merhan = document.getElementById("merhan");
+    var fady = document.getElementById("fady");
+    var mohsen = document.getElementById("mohsen");
+    var sayed = document.getElementById("sayed");
+    var mali = document.getElementById("mali");
+    var osama = document.getElementById("osama");
+    var omsaleh = document.getElementById("omsaleh");
+    var ahsaleh = document.getElementById("ahsaleh");
+    var nashaat = document.getElementById("nashaat");
+    var walid = document.getElementById("walid");
+    var heba = document.getElementById("heba");
+    var jo = document.getElementById("jo");
+    var sameh = document.getElementById("sameh");
+    var swefy = document.getElementById("swefy");
+    var azoz = document.getElementById("azoz");
+    var sherif = document.getElementById("sherif");
+    var wael = document.getElementById("wael");
+    var roaa = document.getElementById("roaa");
+    var yaser = document.getElementById("yaser");
+    var fawzy = document.getElementById("fawzy");
+    var hasan = document.getElementById("hasan");
+    var saad = document.getElementById("saad");
+    var nasef = document.getElementById("nasef");
+    var amany = document.getElementById("amany");
+    var menna = document.getElementById("menna");
+    var shaoib = document.getElementById("shaoib");
+    var marina = document.getElementById("marina");
+
+    /*
+    drhossam.style.display = "none";
+    nilay.style.display = "none";
+    sarhan.style.display = "none";
+    ramy.style.display = "none";
+    asar.style.display = "none";
+    rania.style.display = "none";
+    ingy.style.display = "none";
+    randa.style.display = "none";
+    mona.style.display = "none";
+    mai.style.display = "none";
+    maha.style.display = "none";
+    merhan.style.display = "none";
+    fady.style.display = "none";
+    mohsen.style.display = "none";
+    sayed.style.display = "none";
+    mali.style.display = "none";
+    osama.style.display = "none";
+    omsaleh.style.display = "none";
+    ahsaleh.style.display = "none";
+    nashaat.style.display = "none";
+    walid.style.display = "none";
+    heba.style.display = "none";
+    jo.style.display = "none";
+    sameh.style.display = "none";
+    swefy.style.display = "none";
+    azoz.style.display = "none";
+    sherif.style.display = "none";
+    wael.style.display = "none";
+    roaa.style.display = "none";
+    yaser.style.display = "none";
+    fawzy.style.display = "none";
+    hasan.style.display = "none";
+    saad.style.display = "none";
+    nasef.style.display = "none";
+    amany.style.display = "none";
+    menna.style.display = "none";
+    shaoib.style.display = "none";
+    marina.style.display = "none";
+    */
+
+
+    function myFunctionnilay() {
+
+        if (nilay.style.display === "block") {
+          nilay.style.display = "none";
+        } else {
+          nilay.style.display = "block";
+        }
+        
+        }
+
+    function myFunctiondrhossam() {
+
+        if (drhossam.style.display === "block") {
+            drhossam.style.display = "none";
+        } else {
+            drhossam.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionsarhan() {
+
+        if (sarhan.style.display === "block") {
+            sarhan.style.display = "none";
+        } else {
+            sarhan.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionramy() {
+
+        if (ramy.style.display === "block") {
+            ramy.style.display = "none";
+        } else {
+            ramy.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionasar() {
+
+        if (asar.style.display === "block") {
+            asar.style.display = "none";
+        } else {
+            asar.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionrania() {
+
+        if (rania.style.display === "block") {
+            rania.style.display = "none";
+        } else {
+            rania.style.display = "block";
+        }
+        
+        }
+
+    function myFunctioningy() {
+
+        if (ingy.style.display === "block") {
+            ingy.style.display = "none";
+        } else {
+            ingy.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionranda() {
+
+        if (randa.style.display === "block") {
+            randa.style.display = "none";
+        } else {
+            randa.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionmona() {
+
+        if (mona.style.display === "block") {
+            mona.style.display = "none";
+        } else {
+            mona.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionmai() {
+
+        if (mai.style.display === "block") {
+            mai.style.display = "none";
+        } else {
+            mai.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionmaha() {
+
+        if (maha.style.display === "block") {
+            maha.style.display = "none";
+        } else {
+            maha.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionmerhan() {
+
+        if (merhan.style.display === "block") {
+            merhan.style.display = "none";
+        } else {
+            merhan.style.display = "block";
+        }
+        
+        }
+    function myFunctionfady() {
+
+        if (fady.style.display === "block") {
+            fady.style.display = "none";
+        } else {
+            fady.style.display = "block";
+        }
+        
+        }
+        
+    function myFunctionmohsen() {
+
+        if (mohsen.style.display === "block") {
+            mohsen.style.display = "none";
+        } else {
+            mohsen.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionsayed() {
+
+        if (sayed.style.display === "block") {
+            sayed.style.display = "none";
+        } else {
+            sayed.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionmali() {
+
+        if (mali.style.display === "block") {
+            mali.style.display = "none";
+        } else {
+            mali.style.display = "block";
+        }
+        
+        }
+
+
+    function myFunctionosama() {
+
+        if (osama.style.display === "block") {
+            osama.style.display = "none";
+        } else {
+            osama.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionomsaleh() {
+
+        if (omsaleh.style.display === "block") {
+            omsaleh.style.display = "none";
+        } else {
+            omsaleh.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionahsaleh() {
+
+        if (ahsaleh.style.display === "block") {
+            ahsaleh.style.display = "none";
+        } else {
+            ahsaleh.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionnashaat() {
+
+        if (nashaat.style.display === "block") {
+            nashaat.style.display = "none";
+        } else {
+            nashaat.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionwalid() {
+
+        if (walid.style.display === "block") {
+            walid.style.display = "none";
+        } else {
+            walid.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionheba() {
+
+
+        if (heba.style.display === "block") {
+            heba.style.display = "none";
+        } else {
+            heba.style.display = "block";
+        }
+        
+        }
+        
+    function myFunctionjo() {
+    
+        if (jo.style.display === "block") {
+            jo.style.display = "none";
+        } else {
+            jo.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionsameh() {
+
+        if (sameh.style.display === "block") {
+            sameh.style.display = "none";
+        } else {
+            sameh.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionswefy() {
+
+        if (swefy.style.display === "block") {
+            swefy.style.display = "none";
+        } else {
+            swefy.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionazoz() {
+
+        if (azoz.style.display === "block") {
+            azoz.style.display = "none";
+        } else {
+            azoz.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionsherif() {
+
+        if (sherif.style.display === "block") {
+            sherif.style.display = "none";
+        } else {
+            sherif.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionwael() {
+
+        if (wael.style.display === "block") {
+            wael.style.display = "none";
+        } else {
+            wael.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionroaa() {
+
+
+        if (roaa.style.display === "block") {
+            roaa.style.display = "none";
+        } else {
+            roaa.style.display = "block";
+        }
+        
+        }
+        
+    function myFunctionyaser() {
+    
+        if (yaser.style.display === "block") {
+            yaser.style.display = "none";
+        } else {
+            yaser.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionfawzy() {
+
+        if (fawzy.style.display === "block") {
+            fawzy.style.display = "none";
+        } else {
+            fawzy.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionhasan() {
+
+        if (hasan.style.display === "block") {
+            hasan.style.display = "none";
+        } else {
+            hasan.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionsaad() {
+
+        if (saad.style.display === "block") {
+            saad.style.display = "none";
+        } else {
+            saad.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionnasef() {
+
+        if (nasef.style.display === "block") {
+            nasef.style.display = "none";
+        } else {
+            nasef.style.display = "block";
+        }
+        
+        }
+
+    function myFunctionamany() {
+
+        if (amany.style.display === "block") {
+            amany.style.display = "none";
+        } else {
+            amany.style.display = "block";
+        }
+    
+        }
+
+    function myFunctionmenna() {
+
+
+        if (menna.style.display === "block") {
+            menna.style.display = "none";
+        } else {
+            menna.style.display = "block";
+        }
+
+    }
+
+    function myFunctionshaoib() {
+
+        if (shaoib.style.display === "block") {
+            shaoib.style.display = "none";
+        } else {
+            shaoib.style.display = "block";
+        }
+
+    }
+    
+    function myFunctionmarina() {
+    
+    
+    if (marina.style.display === "block") {
+        marina.style.display = "none";
+    } else {
+        marina.style.display = "block";
+    }
+    
+    }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+  
+
+
+
+
+
